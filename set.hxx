235de01ada1796fe89c959920f3c3d6b06510fe8
curdog <curdog@gmail.com>
2013-02-12 23:00:06 -0500

    empty message

--- set.hxx
+++ set.hxx
@@ -3,18 +3,18 @@
 
 #include "node.hxx"
 
-template <kind> class Sll {
+template <kind> class Set {
 public:
   virtual Sll(kind* head = 0 );
   virtual ~Sll();
+
   kind* next();
   void add( kind, int );
   int rm( );
   kind* cur( );
-  int curIndex( );
   void retToHead();
-
   bool search( kind );
+
 protected:
   node<kind>* head;
   node<kind>* cur;
