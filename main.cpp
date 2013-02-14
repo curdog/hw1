8ba760158b73ed2a87a9bc65e966f4c522ebadb3
bpnikehoops93 <l.miller3@hotmail.com>
2013-02-14 11:42:15 -0500

    added the switch case and input menu

--- main.cpp
+++ main.cpp
@@ -2,7 +2,48 @@
 //change to set.hxx when I change the file names and whatnot
 #include "set.hxx"
 #include <iostream>
-//test
-int main( ){
+
+int main(){
+  
+  /*creating an object called obj*/
+  Node obj;
+  
+  /*displays menu*/
+  cout << "1. Insert\n";
+  cout << "2. Search\n";
+  cout << "3. Remove Current Node";
+  cout << "4. Display List";
+  cout << "5. Quit";
+  cout << "Selection: ";
+  
+  /*reads input*/
+  cin >> choice;
+  
+  /*switch case for the menu*/
+  switch(choice)
+  {
+    case 1:
+      add();
+      break;
+      
+    case 2: 
+      search();
+      break;
+      
+    case 3: 
+      rm();
+      break;
+      
+    case 4: 
+      /*operation overload*/
+      break;
+    
+    case 5:
+      return 0;
+      
+    default:
+      cout << "Error: Please enter 1,2,3,4 or 5";
+  }/*end switch case*/ 
+  
   return 0;
 }
