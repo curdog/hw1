54cf87a78eb54c8ba01dcb4b86864c578b816d91
curdog <curdog@gmail.com>
2013-02-20 01:55:34 -0500

    Fixing comments

--- node.hpp
+++ node.hpp
@@ -1,31 +1,36 @@
 // CLASS PROVIDED:  Node 
 //
 // CONSTRUCTOR for the Node class:
-//   Node( <prim>)
+//   Node( <>)
 //     Description:  	Initializes a new node with data
 //     Preconditions:  none
 //     Postcondition:  New Node with data as well as a next Node = NULL
 //
+//   Node( <>, Node<>)
+//     Description:  	Initializes a new node with data and a next node
+//     Preconditions:  none
+//     Postcondition:  New Node with data as well as a next Node
+//
 // MEMBER FUNCTIONS for the Node class
 //   void setNext (const string value)
-//     Description:  	set the first name to default
-//     Precondition:  value <= 15 characters and no spaces
-//     Postcondition:  first name has been set to value
+//     Description:  	
+//     Precondition:  
+//     Postcondition:  
 //
-//   void setDatas (const string value)
-//     Description:  	set the last name to default
-//     Precondition:  value <= 15 characters and no spaces
-//     Postcondition:  last name set to value
-//
-//   void getData  (const int value)
-//     Description:  	set the hours worked
-//     Precondition:  value >=0 and <=50
-//     Postcondition:  hours worked set to value
-//
-//   void display  (const float value)
-//     Description:  	set the hourly rate
+//   Node<> getNext  (const float value)
+//     Description:  	
 //     Precondition:  value >=6.50 and <=25.75
 //     Postcondition:  hourly rate set to value
+//
+//   void setDatas (const string value)
+//     Description:  	
+//     Precondition:  
+//     Postcondition:  
+//
+//   void getData  (const int value)
+//     Description:  	
+//     Precondition:  
+//     Postcondition: 
 //
 
 
